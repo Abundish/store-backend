ade653df47480e8571ecd6a50b1ba0affeca4830
SamIron0 <samuelironkwec@gmail.com>
2026-04-05 07:10:21 +0100

    Refactor abundish fulfillment module to simplify exports by directly exporting the service, removing unnecessary module definition.

--- src/modules/abundish-fulfillment/index.ts
+++ src/modules/abundish-fulfillment/index.ts
@@ -1,8 +1,2 @@
-import { Module } from "@medusajs/framework/utils"
-import AbundishFulfillmentProviderService from "./service"
-
-export const ABUNDISH_FULFILLMENT_MODULE = "abundish-fulfillment"
-
-export default Module(ABUNDISH_FULFILLMENT_MODULE, {
-    service: AbundishFulfillmentProviderService,
-})
+// src/modules/abundish-fulfillment/index.ts
+export { default } from "./service"
